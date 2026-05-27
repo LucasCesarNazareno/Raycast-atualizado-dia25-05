d1b20a5e32ce41e74c9a046c27518902b032a610
unknown <3jd@e041edu.local>
2026-05-27 08:30:48 -0300

    atualizado dia 27

--- Assets/Scripts/Ray.cs
+++ Assets/Scripts/Ray.cs
@@ -5,14 +5,15 @@ public class RaycastsController : MonoBehaviour
 {
     public float distancia = 100f;
     private LineRenderer lineRenderer;
-    public float tempoExibicao = 0.2f;
-
+    public float tempoExibicao = 0.1f; 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.startWidth = 0.05f;
-        lineRenderer.endWidth = 0.05f;
+
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
         lineRenderer.positionCount = 0;
+
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.startColor = Color.red;
         lineRenderer.endColor = Color.red;
@@ -20,67 +21,62 @@ public class RaycastsController : MonoBehaviour
 
     void Update()
     {
-        // 1. CLIQUE DO MOUSE (Destrói)
+        Vector3 origem = Camera.main.transform.position;
+        Vector3 direcao = Camera.main.transform.forward;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, distancia))
-            {
-                DesenharLaser(ray.origin, hit.point);
-                Destroy(hit.collider.gameObject);
-            }
-            else
-            {
-                DesenharLaser(ray.origin, ray.origin + ray.direction * distancia);
-            }
+            ProcessarRaycast(origem, direcao, "destruir");
         }
 
-        // 2. TECLA T (MODIFICADO: Cria um cubo novo no ponto de impacto)
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, distancia))
-            {
-                DesenharLaser(transform.position, hit.point);
-
-                // Cria o cubo nativo da Unity
-                GameObject novoCubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-                // Posiciona o cubo ligeiramente acima do ponto de impacto para não afundar no chão
-                novoCubo.transform.position = hit.point + hit.normal * 0.5f;
-
-                Debug.Log("Cubo criado em: " + novoCubo.transform.position);
-            }
-            else
-            {
-                DesenharLaser(transform.position, transform.position + transform.forward * distancia);
-
-                // Se o raio não bater em nada, cria o cubo no ar no limite da distância
-                Vector3 posicaoFinal = transform.position + transform.forward * distancia;
-                GameObject novoCubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                novoCubo.transform.position = posicaoFinal;
-            }
-        }
-
-        // 3. TECLA F (Mantém o Destruir)
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, distancia))
+            ProcessarRaycast(origem, direcao, "construir");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ProcessarRaycast(origem, direcao, "pintar");
+        }
+    }
+
+    void ProcessarRaycast(Vector3 origem, Vector3 direcao, string acao)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(origem, direcao, out hit, distancia))
+        {
+            DesenharLaser(origem, hit.point);
+
+            switch (acao)
             {
-                DesenharLaser(transform.position, hit.point);
-                Destroy(hit.collider.gameObject);
+                case "destruir":
+                    Destroy(hit.collider.gameObject);
+                    break;
+
+                case "construir":
+                    GameObject novoCubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    novoCubo.transform.position = hit.point + hit.normal * 0.5f;
+                    break;
+
+                case "pintar":
+                    Renderer rend = hit.collider.GetComponent<Renderer>();
+                    if (rend != null) rend.material.color = Color.blue;
+                    break;
             }
-            else
-            {
-                DesenharLaser(transform.position, transform.position + transform.forward * distancia);
-            }
+        }
+        else
+        {
+            DesenharLaser(origem, origem + direcao * distancia);
         }
     }
 
     void DesenharLaser(Vector3 inicio, Vector3 fim)
     {
-        lineRenderer.positionCount = 2;
+        lineRenderer.positionCount = 3;
         lineRenderer.SetPosition(0, inicio);
         lineRenderer.SetPosition(1, fim);
+
         CancelInvoke(nameof(ApagarLaser));
         Invoke(nameof(ApagarLaser), tempoExibicao);
     }
