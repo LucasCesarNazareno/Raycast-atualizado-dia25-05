59bfe350c2afdf34b5e531ba553f79ebf9793047
Lucas <lucas.cn3007@gmail.com>
2026-06-10 08:00:51 -0300

    projeto pronto

--- Assets/Scripts/Ray.cs
+++ Assets/Scripts/Ray.cs
@@ -1,22 +1,18 @@
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class RaycastsController : MonoBehaviour
 {
-    public float distancia = 100f;
-    private LineRenderer lineRenderer;
-    public float tempoExibicao = 0.1f; 
+    private float distancia = 100f;
+
+    private LineRenderer linha;
+
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        linha = GetComponent<LineRenderer>();
 
-        lineRenderer.startWidth = 0.02f;
-        lineRenderer.endWidth = 0.02f;
-        lineRenderer.positionCount = 0;
-
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.red;
+        linha.startWidth = 0.02f;
+        linha.endWidth = 0.02f;
+        linha.positionCount = 0;
     }
 
     void Update()
@@ -26,63 +22,47 @@ public class RaycastsController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            ProcessarRaycast(origem, direcao, "destruir");
+            RaycastHit hit;
+
+            if (Physics.Raycast(origem, direcao, out hit, distancia))
+            {
+                if (hit.collider.CompareTag("alvo"))
+                {
+                Destroy(hit.collider.gameObject);
+                }
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            ProcessarRaycast(origem, direcao, "construir");
+            RaycastHit hit;
+
+            if (Physics.Raycast(origem, direcao, out hit, distancia))
+            {
+                GameObject cubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cubo.transform.position = hit.point + hit.normal * 0.5f;
+                cubo.tag = "alvo";
+                Renderer objetoRenderer = cubo.GetComponent<Renderer>();
+                if (objetoRenderer != null)
+                {
+                    objetoRenderer.material.color = Color.red;
+                }
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            ProcessarRaycast(origem, direcao, "pintar");
-        }
-    }
+            RaycastHit hit;
 
-    void ProcessarRaycast(Vector3 origem, Vector3 direcao, string acao)
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(origem, direcao, out hit, distancia))
-        {
-            DesenharLaser(origem, hit.point);
-
-            switch (acao)
+            if (Physics.Raycast(origem, direcao, out hit, distancia))
             {
-                case "destruir":
-                    Destroy(hit.collider.gameObject);
-                    break;
+                Renderer objeto = hit.collider.GetComponent<Renderer>();
 
-                case "construir":
-                    GameObject novoCubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    novoCubo.transform.position = hit.point + hit.normal * 0.5f;
-                    break;
-
-                case "pintar":
-                    Renderer rend = hit.collider.GetComponent<Renderer>();
-                    if (rend != null) rend.material.color = Color.blue;
-                    break;
+                if (objeto != null)
+                {
+                    objeto.material.color = Color.blue;
+                }
             }
         }
-        else
-        {
-            DesenharLaser(origem, origem + direcao * distancia);
-        }
-    }
-
-    void DesenharLaser(Vector3 inicio, Vector3 fim)
-    {
-        lineRenderer.positionCount = 3;
-        lineRenderer.SetPosition(0, inicio);
-        lineRenderer.SetPosition(1, fim);
-
-        CancelInvoke(nameof(ApagarLaser));
-        Invoke(nameof(ApagarLaser), tempoExibicao);
-    }
-
-    void ApagarLaser()
-    {
-        lineRenderer.positionCount = 0;
     }
 }
